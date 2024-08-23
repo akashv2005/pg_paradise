33e9fd9ebfb7ebeea227e749a823f590ca48cdbe
Akash Verma <akashverma10200@gmail.com>
2024-08-23 14:03:24 +0530

    Changed card direction

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ properties.forEach(property => {
                 <div class="p-4 pb-0">
                     <h5 class="text-primary mb-3">${property.price}</h5>
                     <p><i class="fa fa-map-marker-alt text-primary me-2"></i>${property.location}</p>
-                    <a href="#"><p><i class="fa fa-map-marker-alt text-primary me-2"></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                    <a href="#" id="directions"><p><i class="fa fa-map-marker-alt text-primary me-2"></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
                    <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
                  </svg> Directions</p></a>
                 </div>
